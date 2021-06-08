d45d2de0bf365c178aaf867e900b53a7fc899577
Blaise <blaise.carnevillier@orange.com>
2021-06-08 11:19:01 +0200

    remove debug

--- client/src/components/NewRole.js
+++ client/src/components/NewRole.js
@@ -94,15 +94,10 @@ class NewRole extends React.Component {
      * Create a new Role if Isnew or update an already existing Role
      * For now all addresses are hard coded  
      */
-    //TODO Add the addresses to the SC call 
     async manageRole() {
 
         if (this.state.isNew) {
-            const address = '0xB075d6DA74408C291c86c0e651dd10e962efc82D'
             try {
-                console.log(this.state.address);
-                console.log(this.state.name);
-                console.log(this.state.isAdmin);
                 const res = await this.state.instance.methods.newRole(this.state.address, this.state.name, !this.state.isAdmin).send({ from: this.state.accounts[0] })
             }
             catch {
@@ -126,8 +121,8 @@ class NewRole extends React.Component {
 
         if (this.state.isNew) {
             address.push(
-<>
-<div class="form-group col-12 col-lg-6">
+                <>
+                    <div class="form-group col-12 col-lg-6">
                         <label for="phone" class="is-required">Address</label>
                         <input type="input" name="address" class="form-control required" onInput={e => this.onChange(e)} onChange={e => this.onChange(e)} required aria-required="true"  ></input>
                     </div>
